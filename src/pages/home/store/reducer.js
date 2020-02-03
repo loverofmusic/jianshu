2594780056bd12130d4e442c13479737019ab739
loverofmusic <1073972000@qq.com>
2020-02-03 23:46:28 +0800

    8.9 返回顶部3

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -18,15 +18,20 @@ const changeHomeData = (state, action) => {
   });
 };
 
+const addArticleList = (state, action) => {
+  return state.merge({
+    articleList: state.get("articleList").concat(action.list),
+    articlePage: action.nextPage
+  });
+}
+
+
 export default (state = defaultState, action) => {
   switch (action.type) {
     case constants.CHANGE_HOME_DATA:
       return changeHomeData(state, action);
     case constants.ADD_ARTICLE_LIST:
-      return state.merge({
-        articleList: state.get("articleList").concat(action.list),
-        articlePage: action.nextPage
-      });
+      return addArticleList(state, action);
     case constants.TOGGLE_SCROLL_TOP:
       return state.set("showScroll", action.show);
     default:
