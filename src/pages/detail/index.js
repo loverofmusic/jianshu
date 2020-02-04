b68dffb9d7e657d26424063c555a3d3a08329be2
loverofmusic <1073972000@qq.com>
2020-02-04 14:25:20 +0800

    redux管理详情页面数据2

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { DetailWrapper, Header, Content } from "./styled";
+
 class Detail extends Component {
   render() {
     return (
       <DetailWrapper>
-        <Header></Header>
-        <Content>
-          
-         </Content>
+        <Header>{this.props.title}</Header>
+        <Content dangerouslySetInnerHTML={{__html: this.props.content}}/>
       </DetailWrapper>
     );
   }
 }
 
-export default Detail;
+const mapState = state => ({
+  title: state.getIn(["detail", "title"]),
+  content: state.getIn(["detail", "content"])
+});
+
+export default connect(mapState, null)(Detail);
