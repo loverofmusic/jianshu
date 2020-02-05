2a1d9ca463dc47f55dec8eddd20a6af0f0cd6bf0
loverofmusic <1073972000@qq.com>
2020-02-05 23:39:40 +0800

    9.5 登录页面布局

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import Detail from "./pages/detail";
 import Home from "./pages/home";
+import Login from './pages/login'
 
 class App extends Component {
   render() {
@@ -21,6 +22,7 @@ class App extends Component {
           <Route path="/" exact component={Home}></Route>
           {/* <Route path="/detail/:id" exact render={() => <Detail></Detail>}></Route> */}
           <Route path="/detail/:id" exact component={Detail}></Route>
+          <Route path="/login" exact component={Login}></Route>
 
         </BrowserRouter>
       </Provider>
---
+++ src/pages/login/index.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import { LoginWrapper, LoginBox, Input, Button } from "./style";
+
+class Login extends PureComponent {
+  render() {
+    return (
+      <LoginWrapper>
+        <LoginBox>
+          <Input placeholder="账号" />
+          <Input placeholder="密码" />
+          <Button>登录</Button>
+        </LoginBox>
+      </LoginWrapper>
+    );
+  }
+}
+
+const mapState = state => ({
+  title: state.getIn(["detail", "title"]),
+  content: state.getIn(["detail", "content"])
+});
+
+const mapDispatch = dispatch => ({});
+
+export default connect(mapState, mapDispatch)(Login);
